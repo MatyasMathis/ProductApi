533c271aba4e156e75f523cbbf5132c5297f4848
matyasmathis <matyasmathis0@gmail.com>
2023-03-25 20:00:33 +0200

    Created Models and DBMigrations

---
+++ ProductApi/Data/ProductApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Models;
+
+namespace ProductApi.Data
+{
+    public class ProductApiDbContext:DbContext
+    {
+        public ProductApiDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- ProductApi/Program.cs
+++ ProductApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ProductApiDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ProductApiConnString")));
 
 var app = builder.Build();
 
